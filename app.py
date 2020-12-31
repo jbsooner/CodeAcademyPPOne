a692a923138bfaa460285eede70ad20ec512fef8
jbsooner <55551077+jbsooner@users.noreply.github.com>
2020-12-31 16:50:57 -0600

    Updated Dictonary Function
    Added a dictionary to push function results to.

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ import datetime
 
 df = pd.read_csv('insurance.csv')
 
+# tabulet table format
 tabul_format = 'pretty'  # https://pypi.org/project/tabulate/ -- go here for different table formats
 
 print('The current data and time is: ', datetime.datetime.now())
 print('===============================================================================================================')
 print('Global Data')
 
+# Getting general data from the dataset
 # Create a global_list for the variables below to be appended to / created a header list for the tablulate table
 global_list = []
 header_list_global = ['Avg Age', 'Total Items', 'Total Smokers', 'Total Insurance Costs', 'Total BMI', 'Total Avg Costs', 'Total Avg BMI']
@@ -43,62 +45,9 @@ global_list_total.append(tabulate_end_list)
 print(tabulate(global_list_total, headers=header_list_global, tablefmt=tabul_format, floatfmt='.2f'))
 print('===============================================================================================================')
 
-
-print('====================================================================================================================')
-print('Data by Region')
-
-# Get the average of a column
-def average(item):
-    average_item = df[item].mean()
-    return round(average_item)
-
-region_dict = {}
-
-
-def region_count(items):
-    for val, cnt in df[items].value_counts().iteritems():
-        region_dict[val] = cnt
-    return region_dict
-
-
-
-def region_data(which_region):
-    current_region = df[df['region'].isin([which_region])]
-    current_region_mean_age = round(current_region['age'].mean())
-    current_region_count = current_region['region'].count()
-    smoker_count = current_region['smoker'] == 'yes'
-    total_insurance_costs = current_region['charges'].sum()
-    total_bmi = current_region['bmi'].sum()
-    average_cost_region = total_insurance_costs / current_region_count
-    average_bmi = total_bmi / current_region_count
-    average_children = current_region['children'].mean()
-    
-    
-    return which_region, current_region_mean_age, current_region_count, sum(smoker_count), round(total_insurance_costs), round(average_cost_region), round(average_bmi, 2), round(average_children,2)
-
-
-region_count('region')
-
-header_list = ['Region', 'Avg Age', 'Region Count', 'Smoker Count', 'Total Insurance Cost', 'Avg Cost', 'Avg BMI', 'Average Children']
-
-total_list = []
-for key in region_dict.keys():
-    region_data(key)
-    # print('The {} region has an average age of {}, count of {}, {} smokers, {} total insurance costs, average cost of {}, and average bmi of {}'.format(key, region_data(key)[1], region_data(key)[2], region_data(key)[3], region_data(key)[4], region_data(key)[5], region_data(key)[6]))
-    new_list = []
-    new_list.append(key)
-    new_list.append(region_data(key)[1])
-    new_list.append(region_data(key)[2])
-    new_list.append(region_data(key)[3])
-    new_list.append(region_data(key)[4])
-    new_list.append(region_data(key)[5])
-    new_list.append(region_data(key)[6])
-    new_list.append(region_data(key)[7])
-    total_list.append(new_list)
-
-print(tabulate(total_list, headers=header_list, tablefmt=tabul_format, floatfmt='.2f'))
-print('====================================================================================================================')
 print('===============================================================================================================')
+
+results_dict = {}
 
 # This function will allow us to get stats for any column value we want for example are they a smoker
 def sort_by_col(column, value):
@@ -107,20 +56,28 @@ def sort_by_col(column, value):
     # print('Global', value.capitalize(), 'Data')
     if data_col == 'age':
         print('Global', data_col.capitalize(), 'Data')
+        dict_key = 'Global {} Data'.format(data_col.capitalize())
     elif data_col == 'sex' and value_to_lookup == 'female':
         print('Global Female Data')
+        dict_key = 'Global Female Data'
     elif data_col == 'sex' and value_to_lookup == 'male':
         print('Global Male Data')
+        dict_key = 'Global Male Data'
     elif data_col == 'children':
         print('Number of Children', value_to_lookup)
+        dict_key = 'Number of Children {}'.format(value_to_lookup)
     elif data_col == 'smoker' and value_to_lookup == 'yes':
         print('Is a Smoker')
+        dict_key = 'Is a Smoker'
     elif data_col == 'smoker' and value_to_lookup == 'no':
         print('Is not a Smoker')
+        dict_key = 'Is not a Smoker'
     elif data_col == 'region':
         print('Global', value_to_lookup.capitalize(), 'Data')
+        dict_key = 'Global {} Data'.format(value_to_lookup.capitalize())
     elif data_col == 'charges':
         print('Global Charges Data')
+        dict_key = 'Global Charges Data'
 
     # Create a DataFrame for the item we want to inspect.
     new_df = df[(df[data_col] == value_to_lookup)]
@@ -155,10 +112,17 @@ def sort_by_col(column, value):
     tabulate_list = []
     tabulate_list.append(list_item)
     tabulate_list.append(tabulate_end_list_smoker)
+    
+    data_dict = {dict_key:[age_mean, items_total, smoker_item_total, item_total_cost, item_bmi_total, item_avg_cost, item_avg_bmi]}
+    results_dict.update(data_dict)
 
     # print out the tabulate table
     print(tabulate(tabulate_list, headers=header_list_global_smoker, tablefmt=tabul_format, floatfmt='.2f'))
     print('===============================================================================================================')
     
-sort_by_col('children', 0)
+
 sort_by_col('region', 'southwest')
+sort_by_col('region', 'northwest')
+sort_by_col('sex', 'female')
+sort_by_col('sex', 'male')
+sort_by_col('children',3)
